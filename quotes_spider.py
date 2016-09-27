bd76390cea645cf31b11461438d77575818c8a42
Brayan <bsalas.0694@gmail.com>
2016-09-27 10:55:32 -0600

    Ejemplo de Scrapy

---
+++ quotes_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+	name = "quotes"
+	start_urls = [
+		'http://quotes.toscrape.com/page/1/',
+		'http://ecci.ucr.ac.cr/',
+	]
+
+	def parse(self, response):
+		page = response.url.split("/")[-2]
+		filename = 'quotes-%s.html' % page 
+		with open(filename, 'wb') as f:
+		f.write(response.body)
+
+		for quote in response.css('div.quote'):
+			yield {
+				'text': quote.css('span.text::text').extract_first(),
+				'author': quote.css('span small::text').extract_first(),
+				'tags': quote.css('div.tags a.tag::text').extract(),
+			}
+
+		next_page = response.css('li.next a::attr(href)').extract_first()
+		if next_page is not None:
+			next_page = response.urljoin(next_page)
+			yield scrapy.Request(next_page, callback=self.parse)
