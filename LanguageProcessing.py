9bd5e337f86131a28d8149e4b6da90121aa73a59
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-09-28 19:58:37 -0600

    Se cambia el nombre de la clase de parser -> LanguageProcessing para una mejor semantica

--- LanguageProcessing.py
+++ LanguageProcessing.py
@@ -13,29 +13,29 @@ Tarea programada 1
 
 from nltk.stem.porter import *   # Modulo con el algoritmo de Porter para hacer stemming
 
-# ========================
-#       Clase Parser
-# ========================
+# ===================================================
+#       Clase del procesamiento linguistico
+# ===================================================
 
-class Parser(object):
-    """Clase que contiene los metodos necesarios para parsear documentos"""
+class LanguageProcessing(object):
+    """Clase que contiene los metodos necesarios para realizar el procesamiento linguistico de documentos"""
 
     @staticmethod
-    def porter(path):
+    def Porter(path):
         """
         Metodo estatico que se encarga de normalizar y parsear un documento con el algoritmo de Porter
             * Recibe como parametro la ruta del documento que va a parsear
         """
-        porter = PorterStemmer()
+        porter_stemmer = PorterStemmer()
         doc = open(path, 'r')
         result = open('p_'+path, 'a')   # TODO: ELIMINAR
         # result = '' # TODO: descomentar
         for line in doc:
             for word in line.split(' '):    # Se ocupa usar split(' ') para que solo haga split por espacios, si se usa split() tambien cuenta los cambios de linea
                 # result += porter.stem(word).lower() if word.endswith('\n') else porter.stem(word).lower() + ' ' # TODO: descomentar
-                result.write(porter.stem(word).lower()) if word.endswith('\n') else result.write(porter.stem(word).lower()+' ')     # TODO: ELIMINAR
+                result.write(porter_stemmer.stem(word).lower()) if word.endswith('\n') else result.write(porter_stemmer.stem(word).lower()+' ')     # TODO: ELIMINAR
         doc.close()
         result.close()
         # result # TODO: descomentar
 
-Parser.porter('prueba.txt')
+LanguageProcessing.Porter('prueba.txt')
