0c3dd1a1bd038dcdb84ea845c01074d6645b1603
Brayan <bsalas.0694@gmail.com>
2016-10-02 12:24:16 -0600

    Arreglo de nombre. Convenio

--- buscador/buscador/spiders/buscador.py
+++ buscador/buscador/spiders/buscador.py
@@ -1,27 +1,27 @@
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.item import Item, Field
-
-
-class MyItem(Item):
-    url= Field()
-
+import os.path
 
 class Buscador(CrawlSpider):
     name = 'buscador'
     allowed_domains = ['ucr.ac.cr']
-    start_urls = ['http://www.ecci.ucr.ac.cr/','http://www.ucr.ac.cr/']
+    start_urls = [
+        'http://www.ecci.ucr.ac.cr/',
+        'http://www.ci.ucr.ac.cr/',
+        'http://www.ucr.ac.cr/',
+        'http://www.universidadescr.com/',
+    ]
 
-    rules = (Rule(SgmlLinkExtractor(), callback='parse_url', follow=False), )
+    rules = (
+        Rule(SgmlLinkExtractor(), callback='parse_url', follow=False), 
+    )
 
     def parse_url(self, response):
 	# Falta arreglar el nombre de los archivos ya guardado.
-	page = response.url.split("/")[-1]
-	filename = 'docs/ecci_%s.html' % page	
-	with open(filename, 'wb') as f:
-		f.write(response.body)
-		self.log('Archivo %s guardado' % filename)
-
-        item = MyItem()
-        item['url'] = response.url
-        return item
+    	page = response.url.split('/')
+        name_Page = '-'.join(page)
+        filename = 'docs/%s.html' % name_Page
+    	with open(filename, 'wb') as f:
+    		f.write(response.body)
+    		self.log('Archivo %s guardado' % filename)
