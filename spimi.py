bbb29a6435dbb997e609d14c6c6911c1e32538b5
Brayan <bsalas.0694@gmail.com>
2016-10-03 11:31:08 -0600

    SPIMI que genera diccionario

--- spimi.py
+++ spimi.py
@@ -7,28 +7,49 @@ Tarea programada 1
 @author: Fabian Rodriguez B25695
 '''
 
-class SPIMI(token_stream):
+# Se asume que desde donde se invoca es donde se maneja el tamaño del buffer. [Como cuando se llena la memoria]
+def SPIMI(token_stream):
 	# Se utiliza open para crear el archivo donde va a estar el output_file
-	with open("output_spimi.txt","wb") as output_file:
+	with open("spimi.bin","wb") as output_file:
 		dictionary = {}
 		for token in token_stream:
 			# pair[0] => token, pair[1] => doc_id
-			pair = [toke[0],token[1]]
+			pair = [token[0],token[1]]
 			if pair[0] is not dictionary:
-				posting_list = add_to_dictionary(dictionary,pair[0])
+				posting_list = addToDictionary(dictionary,pair[0])
 			else:
-				posting_list = get_posting_list(dictionary,pair[0])
-			add_to_dictionary(posting_list,pair[1])
+				posting_list = getPostingList(dictionary,pair[0])
+			addToPostingsList(posting_list,pair[1])
+		# Ordenar los pares por el término. lambda en python permite hacer una funcion anónima.
+		sorted_terms = sorted(dictionary, key=lambda pair: pair[0], reverse=True)
+		writeBlockToDisk(sorted_terms, output_file)
+		writeBlockToDisk(dictionary, output_file)
+	output_file.close()
 
-def add_to_dictionary(dictionary, term):
+def addToDictionary(dictionary, term):
 	dictionary[term] = []
 	return dictionary[term]
 
-def get_postings_list(dictionary, term): 
+def getPostingsList(dictionary, term): 
 	return dictionary[term]	
 
-def add_to_postings_list(postings_list, doc_id): 
+def addToPostingsList(postings_list, doc_id): 
 	if doc_id not in postings_list:
-		postings_list.insert(0,doc_id)
-	else: 
-		postings_list
+		postings_list.insert(0,doc_id) # Inserta en el inicio de la lista, la posicion que siempre vamos a conocer.
+
+def writeBlockToDisk(file, output):
+	# pickle permite guardar estructuras como binarios, para luego recuperarlas igual. cPickle es más eficiente.
+	try: from cPickle import HIGHEST_PROTOCOL,dump
+	except: from pickle import HIGHEST_PROTOCOL,dump
+
+	dump(file, output_file, HIGHEST_PROTOCOL)
+
+# Metodo que obtiene de un archivo la estructura original del mismo.
+def readBlockFromDisk(input):	
+	try: from cPickle import load
+	except: from pickle import load
+
+	open(input, 'rb') as inp:
+		file = load(inp)
+	inp.close()
+	return file
