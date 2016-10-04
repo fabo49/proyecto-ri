ebf130b28c3bc3f057e6741e49bf77300bc694c6
Brayan <bsalas.0694@gmail.com>
2016-10-03 20:59:32 -0600

    detalles finales en buscador

--- buscador/buscador/spiders/buscador.py
+++ buscador/buscador/spiders/buscador.py
@@ -8,23 +8,24 @@ import re
 
 class Buscador(CrawlSpider):
     name = 'buscador'
+    request_timeout = 120
+    depth_limit = 4
     allowed_domains = ['londonmet.ac.uk']
     start_urls = [
         'http://www.londonmet.ac.uk/',
         'http://www.londonmet.ac.uk/faculties',
         'http://www.londonmet.ac.uk/contact-us/',
-        'http://www.universidadescr.com/',
     ]
 
     rules = (
-        Rule(SgmlLinkExtractor(), callback='parse_url', follow=False), 
+        Rule(SgmlLinkExtractor(allow=()), callback='parse_url', follow=True), 
     )
 
     # Guarda los docs con el nombre de la dir donde se obtuvo. Se utiliza un convenio de nombre '-' por '/'
     def parse_url(self, response):
     	page = response.url.split('/')
         name_Page = "|".join(page)
-        filename = 'docs/%s.html' % name_Page
+        filename = 'docss/%s.html' % name_Page
     	with open(filename, 'wb') as f:
     		f.write(response.body)
     		self.log('Archivo %s guardado satisfactoriamente' % filename)
