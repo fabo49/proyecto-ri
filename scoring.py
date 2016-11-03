c880a888a8dcc749fecb5f438d4288d62a8a7f7b
Brayan <bsalas.0694@gmail.com>
2016-11-03 17:04:46 -0600

    Scoring con cambios

--- scoring.py
+++ scoring.py
@@ -17,35 +17,25 @@ def score(query,ind,dic):
 	dictionary = FileToDictionary(dictionary_file)
 	dictionary_file.close()
 
-	query_terms = tokenize(query)
+	query_tokenize = tokenize(query)
+	query_terms = []
 
-# Rediseñar desde aquí
-	act_term = index.readLine()
-	terms_readed.append(act_term)
-	for term in query:
-		if term in terms_readed:
-			l = getPostingList(term,dictionary)
-			query_terms.append(l)
+	# Se buscan las listas de postings de los distintos terminos.
+	for term in query_tokenize:
+		if term in index:
+			query_terms.append(dictionary[term])
 		else:
-			while act_term not term and act_term not "":
-				act_term = index.readLine()
-				terms_readed.append(act_term)
-			if act_term is term:
-				l = getPostingList(term,dictionary)
-				query_terms.append(l)
-			else:
-				if act_term not term and act_term is "":
-					print "Termino no encontrado"
-# Hasta aquí
+			print "[debug]: Termino no encontrado"
 
 	# Se encuentra el posting_list de menor tamaño
 	posAct = 0
 	posListThin = 0
 	listThin = len(query_terms[0])
 	for posting_list in query_terms:
-		if listThin < len(posting_list):
+		if len(posting_list) < listThin:
 			posListThin = posAct
 			listThin = len(posting_list)
+			posAct += 1
 		else:
 			posAct += 1
 
@@ -54,12 +44,9 @@ def score(query,ind,dic):
 	docsAppears = []
 	for x in xrange(listThin):
 		for lis in query_terms:
-			while 0 < len(lis) and lis[0] < postingListThin[x]:
+			while 0 < len(lis) and lis[0].split(':')[0] < postingListThin[x].split(':')[0]:
 				lis.pop(0)
-			if len(lis) is not 0 and lis[0] = postingListThin[x]:
+			if len(lis) is not 0 and lis[0].split(':')[0] = postingListThin[x].split(':')[0]:
 				if lis[0] not in docsAppears:
 					docsAppears.append(lis[0])
-
-# Busca en el diccionario el postings_list del termino.
-def getPostingList(term,dic):
 	
