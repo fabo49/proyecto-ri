5b70596e1bf905676f54aee1129c540d32ba9e44
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-11-04 16:14:07 -0600

    Se hace la estructura y el diseno de la pagina de resultados

--- hello.py
+++ hello.py
@@ -1,16 +1,28 @@
 # import os # Para correr en C9
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/results")
+
+@app.route('/results/')
+@app.route('/results/', methods=['GET'])
 def results():
-	return "Estos son los resultados encontrados"
+    t_inicial = datetime.now().microsecond
+    # Hacer la logica de la busqueda
+    i = 0
+    for i in xrange(20000):
+        print "[CICLO TONTO]: es nada mas para ver tiempo."
+
+    t_final = datetime.now().microsecond - t_inicial
+    return render_template('results.html', query=request.args.get('query'), time=t_final)
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5052)
+    app.run(debug=True, port=5052)
     # app.run(debug=True,host=os.environ['IP'],port=int(os.environ['PORT'])) # Para correr en C9
-    
