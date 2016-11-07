46b1c5a2f5bb9e3b76b03a3da7f3fc287c563c14
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-11-06 21:38:34 -0600

    Se termina la entidad resultado y cambios menores

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
+'''
+Universidad de Costa Rica
+Escuela de Ciencias de la Computacion e Informatica
+CI-2414: Recuperacion de la informacion
+Tarea programada 1
+@author: Brayan Salas B26050
+@author: Fabian Rodriguez B25695
+'''
+
 # import os # Para correr en C9
 from flask import Flask, render_template, request
 from datetime import datetime
@@ -24,5 +33,6 @@ def results():
 
 
 if __name__ == "__main__":
+    # Aca hay que llamar el crawler, generar el indice y el diccionario
     app.run(debug=True, port=5052)
     # app.run(debug=True,host=os.environ['IP'],port=int(os.environ['PORT'])) # Para correr en C9
